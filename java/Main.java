db870fefb12e0496b81e23ae14b458a8ff95ac08
ImmortalQT <166763759+ImmortalQT@users.noreply.github.com>
2024-05-04 12:48:06 +0530

    Add files via upload

---
+++ java/Main.java
@@ -0,0 +1,18 @@
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        int a=4;
+        int b=4;
+        //arth operators
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+//        scanner sc = new scanner(System.in);
+    }
+}
---
+++ python/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+
+# def g=print(lambda x:x*x*x)
+
+
+# g(2)
