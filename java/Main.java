694f18a5ea98f27bc713d3f0439dbc17837bb8b1
Immortal <Anmollovevarshney@gmail.com>
2024-05-16 12:22:20 +0530

    if else, loops

--- java/Main.java
+++ java/Main.java
@@ -3,16 +3,78 @@
  */
 public class Main {
 
+
     public static void main(String[] args) {
-        System.out.println("hello world");
-        int a=4;
-        int b=4;
-        //arth operators
-        System.out.println(a+b);
-        System.out.println(a-b);
-        System.out.println(a*b);
-        System.out.println(a/b);
-        System.out.println(a%b);
-//        scanner sc = new scanner(System.in);
+////        System.out.println("hello world");
+////        int a=4;
+////        int b=4;
+////        //Arthmatic operators
+////        System.out.println(a+b);
+////        System.out.println(a-b);
+////        System.out.println(a*b);
+////        System.out.println(a/b);
+////        system.out.println(a%b);
+//        // if else statements
+//
+//        int x =5;
+//        int y =10;
+//        if (x<y){
+//            System.out.println("x is less than y");
+//        }
+//        else {
+//            System.out.println("x is greater than y");
+//        }
+//    }
+
+
+//        System.out.println("hello");
+//        int marks = 75 ;
+//        if (marks < 50) {
+//            System.out.println("Fail");
+//        }
+//        else if (marks > 50 && marks < 60) {
+//            System.out.println("D Grade");
+//        }
+//        else if (marks > 60 && marks < 70) {
+//            System.out.println("C Grade");
+//        }
+//        else if (marks > 70 & marks < 80) {
+//            System.out.println("B Grade");
+//        }
+//        else if (marks >80 & marks < 90){
+//            System.out.println("A Grade");
+//        }
+//        else if (marks >90 & marks < 100){
+//            System.out.println("A+ Grade");
+//        }else {
+//            System.out.println("A++ Grade");
+//        }
+
+////      Whileloop Statement
+//        int number = 2;
+//        int i = 1;
+//        System.out.println("Table of 2:");
+//        while (i <= 10) {
+//            System.out.println(number + " * " + i + " = " + (number * i));
+//            i++;
+//        }
+//    }
+////      Forloop Statement
+//        System.out.println("Hello World");
+////        for (int i = 1; i <= 10; i++){
+////            System.out.println(i);
+////        }
+//        int num=2;
+//        for (int i=1; i<=10; i++){
+//            System.out.println(num + "*" + i + "=" + i*2);
+//        }
+
+
+
+
+
     }
 }
+
+
+
