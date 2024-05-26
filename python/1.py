b8fafd70908b41317796b9b4d7955c3193663326
Immortal <Anmollovevarshney@gmail.com>
2024-05-26 12:28:40 +0530

    Re-add python

---
+++ python/1.py
@@ -0,0 +1,323 @@
+# # '''a=1
+# # b=1
+# # #print(a+b)
+# # c=a+b
+# # print(c)
+# # '''
+
+# # a = "Shivani"
+# # print(a,type(a))
+
+# # b= 10
+# # print(b,type(b))
+
+# # c=1.0
+# # print(c,type(c))
+
+# # d=3+4j
+# # print(d,type(d))
+
+# # e=True
+# # print(e,type(e))
+
+# # #Arthmatics Operators
+
+# # a=10
+# # b=20
+# # print(a+b)
+
+# # a=10
+# # b=20
+# # print(a-b)
+
+# # a=10
+# # b=20
+# # print(a*b)
+
+# # a=10
+# # b=20
+# # print(a/b)
+
+# # #Relational Opertors
+
+# # a=10
+# # b=20
+# # print(a<b)
+
+# # a=10
+# # b=20
+# # print(a>b)
+
+# # a=10
+# # b=20
+# # print(a==b)
+
+# # a=10
+# # b=20
+# # print(a!=b)
+
+# # #Logical Operators
+
+# # a=True
+# # b=False
+# # print(a&a)
+
+# # a=True
+# # b=False
+# # print(a&b)
+
+# # a=True
+# # b=False
+# # print(b&a)
+
+# # a=True
+# # b=False
+# # print(a|a)
+
+# # a=True
+# # b=False
+# # print(a|b)
+
+# # print("Hello\nWorld")
+
+# # #Topic String
+# # my="my name is op"
+# # #print first character of a string
+# # print(my[0])
+# # #find length in string function
+# # print(len(my))
+# # #lower case function
+# # print(my.lower())
+# # #upper case function
+# # print(my.upper())
+
+# # #replace function
+# # print(my.replace('is','are'))
+# # #count funtion
+# # print(my.count('m'))
+# # #string find function
+# # print(my.find('is'))
+# # #split function
+# # print(my.split(' '))
+
+# # #Tuple in python
+# # tup1=('a',1,True,'b','c')
+# # tup2=(2,3,4,5)
+# # print(len(tup1))
+# # print(tup1[0])
+# # print(tup1[3])
+# # print(tup1[-1])
+# # print(type(tup1))
+# # print(tup1[1:4])
+# # print(tup1+tup2)
+# # print(tup1*3)
+# # print(tup1*4+tup2)
+# # print(min(tup2))
+# # print(max(tup2))
+
+# # #List in Python
+# # tup2 = ["apple",'mango','banana','orange']
+# # tup2.sort()
+# # print(tup2)
+
+# # tup2 = [1,5,68,8,466,48,4,65,5]
+# # tup2.sort()
+# # print(tup2)
+# # print(len(tup2))
+# # print(tup2[0])
+# # print(tup2[3])
+# # print(tup2[-1])
+# # print(type(tup2))
+# # print(tup2[1:4])
+# # print(tup2+tup2)
+# # print(tup2*3)
+# # print(tup2*4+tup2)
+# # print(min(tup2))
+# # print(max(tup2))
+
+# # dictionary in python {}
+
+# # fruit = {'apple':10, 'orange':20}
+# # fruit2= {'banan':200,'graps':500}
+# # print (fruit,type(fruit))
+# # print (fruit.keys())
+# # print(fruit.values())
+# # fruit['mango']=30
+# # print(fruit)
+# # fruit['mango']=300
+# # print(fruit)
+# # fruit.pop('mango')
+# # print(fruit)
+# # print(fruit2,type(fruit2))
+# # fruit2.update(fruit)
+# # print(fruit2)
+
+# # if else statements
+
+# # a=61
+# # b=60
+# # if a<b:
+# #     print("A is smaller then B")
+# # else:
+# #     print("A is grater then B")
+
+# # a=2000
+# # b=300
+# # c=40
+
+# # if (a>b) & (a>c) :
+# #     print("a is the greatest number")
+# # elif (b>a) & (b>c) :
+# #     print("b is the greatest number")
+# # else:
+# #     print("c is the greatest number")
+
+# # if in tuple()
+
+# # tup1=('a','b','c','d')
+# # print(tup1,type(tup1))
+
+# # if 'z' in tup1:
+# #     print('z is present in tup1')
+# # else:
+# #     print('z is not present in tup1')
+# # in in list[]
+
+# # l1=['a','b','c']
+# # if l1[1]=='b':
+# #     l1[1]='z'
+# #     print(l1,type(l1))
+
+# # if in dictionary{}
+# # d1={'k1':20,'k2':303,'k3':30}
+# # if d1['k3']==30:
+# #     d1['k3']=d1['k3']+100
+# #     print(d1,type(d1))
+
+# # looping statement while Loop
+
+# # i=1
+# # while i<=10:
+# #     print(i)
+# #     i=i+1
+
+# # i=1
+# # n=2
+# # while i<=10:
+# #     print(n,'*',i,'=',n*i)
+# #     i=i+1
+
+# # while with list
+
+# # l1=[1,2,3,4,5]
+# # i=0
+# # while i<len(l1):
+# #     l1[i]=l1[i]+100
+# #     i=i+1
+# #     print(l1)
+
+# # for loop
+
+# # a=['apple','mango','orange']
+# # for i in a:
+# #     print(i)
+
+# # nested for loop
+
+# # a=['apple','mango','orange']
+# # a2=['banja','chini','chai']
+# # for i in a:
+# #     for j in a2:
+# #         print(i,j)
+
+# # input function
+
+# a=int(input("enter the first number\n"))
+# b=int(input("enter the second number\n"))
+# # c=a*b
+# print(a*b)
+# a=int(input("ple. press 1 to see your suprise\n"))
+# if(a==1):
+#     print("\nshivani tu pagal hai re\n        hahahah\n\n         accha sun\n\ntu sach main pagal hai re\n        hahahah")
+# else:
+#     print("ple. press right key")
+
+# # functions
+
+# # def Hello():
+# #     print("hello world")
+
+# # Hello()
+
+# # def add_10(x):
+# #     print(x+10)
+
+# # add_10(2)
+
+# # def odd_even(x):
+# #     if x%2==0:
+# #         print(x,"is even")
+# #     else:
+# #         print(x,"is odd")
+
+# # odd_even(4)
+
+# # def g=print(lambda x:x*x*x)
+
+
+# # g(2)
+
+# # def count1(s): 
+# #  vowels = "AEIOUaeiou" 
+# #  count = 0 
+# #  for c in s:
+   
+# #   if c in vowels:
+    
+# #     count += 1 
+# #     return count 
+# # print(count1('I love India'))
+# list1 = ['M', 'o', 'n', 'k', 'y'] 
+# print("@".join(list1))
+
+
+# def fizzBuzz(n):
+#     i=1
+#     while i<=n:
+#         if i%3==0 and i%5==0:
+#             print("djk")
+#         elif i%3==0 and i%5!=0:
+#             print('Fizz')
+#         elif i%3!=0 and i%5==0:
+#             print('Buzz')
+#         elif i%3!=0 and i%5!=0:
+#             print(i)
+#         i+=1
+
+
+# fizzBuzz(15)
+
+# if __name__ == '__main__':
+#     a = int(input())
+#     b = int(input())
+#     print (a//b)
+#     print(a/b)
+
+    # if __name__ == '__main__':
+    # n = int(input())
+    # for i in range (0,n):
+    #     print (i**2)
+
+
+def is_leap(year):
+    leap = False
+    if year%4==0 and (year%100==0) and year%400==0:
+        leap=True
+    elif not(year%4==0) and not(year%100==0) and not(year%400==0):
+        leap=False
+    else:
+        leap=False
+        
+    return leap
+
+year = int (int(1990))
